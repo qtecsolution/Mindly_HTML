774883b625cfc909237ed62705e58cf76cc09a0b
it27472 <istiak.tushar.qtec@gmail.com>
2022-01-06 17:46:49 +0600

    home banner completed

---
+++ assets/js/circleslide.js
@@ -0,0 +1,12 @@
+var circle= new jCircle({
+    'container': 'circles-container',
+    'circle': 'circle',
+    'mainContent':'main-circle-content',
+    'animateCircles':true,
+    'speed':3,
+    'mainViewStyle':'normal',
+    'minCirclesEffectOver':'pulse',
+    'contentType':'images',
+    'stopOnOverMain':false,
+    'mainContentOverAction':'normal'
+});
