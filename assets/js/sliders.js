6b2f029e469fa4298355ec8d036f0bcc0c085190
it27472 <istiak.tushar.qtec@gmail.com>
2022-01-09 18:49:12 +0600

    achievement section end

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -17,7 +17,6 @@ $(document).ready(function(){
     }
     // CHARACTERISTICS SLIDER END
 
-
     // PACKAGES SLIDER START
     const packages = $('.packages');
     
@@ -48,6 +47,11 @@ $(document).ready(function(){
                     nav: false
                 },
 
+                991 : {
+                    items: 2,
+                    nav: false
+                },
+
                 1024 : {
                     items: 3,
                     nav: true
@@ -64,5 +68,33 @@ $(document).ready(function(){
         $(".owl-next").html('<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M31.875 24.5011L31.875 11.4976C31.875 6.96762 28.6665 4.12512 24.126 4.12512L11.874 4.12512C7.3335 4.12512 4.125 6.95262 4.125 11.4976L4.125 24.5011C4.125 29.0461 7.3335 31.8751 11.874 31.8751L24.126 31.8751C28.6665 31.8751 31.875 29.0461 31.875 24.5011Z" stroke="#409E85" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M24.1291 18.0001L11.8711 18.0001" stroke="#409E85" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M18.4844 12.3781L24.1304 18.0001L18.4844 23.6221" stroke="#409E85" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>');
     }
     // PACKAGES SLIDER END
+
+
+    // DOCTOR LIST SLIDER START
+    const doctors = $('.mobiledoctors');
+    
+    if(doctors.length > 0) {
+        doctors.owlCarousel({
+            margin: 12,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            dots: false,
+            responsive : {
+                // breakpoint from 0 up
+                0 : {
+                    items: 1,
+                    nav: false
+                },
+
+                // breakpoint from 768 up
+                400 : {
+                    items: 2,
+                    nav: false
+                }
+            }
+        });
+    }
+    // DOCTOR LIST SLIDER END
     
 });
