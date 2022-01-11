1f95fef4f05a1513ece30685ab95ca10db3899f2
it27427 <istiak.tushar.qtec@gmail.com>
2022-01-11 12:04:31 +0600

    pages updated

--- assets/js/animations/home-animation.js
+++ assets/js/animations/home-animation.js
@@ -1,31 +1,151 @@
 // HOME PAGE START
 
-TweenMax.staggerFrom(".homeimage", 2, {
+TweenMax.from(".desktopbanner", 1, {
+    opacity: 0,
     delay: .5,
-    opacity: 0,
-    y: -800,
-    ease: Expo.easeInOut
-}, 0.08);
-
-TweenMax.from(".hometitle", 3, {
-    opacity: 0,
-    delay: .8,
-    y: 20,
+    x: 20,
     ease: Expo.easeInOut
 });
 
-TweenMax.from(".hometext", 4, {
+TweenMax.from(".bannerinfo", 2, {
     opacity: 0,
-    delay: .8,
-    y: 20,
+    delay: .75,
+    x: -20,
     ease: Expo.easeInOut
 });
 
-TweenMax.from(".home-button", 5, {
+TweenMax.from(".appointment", 3, {
     opacity: 0,
     delay: 1,
     y: 20,
     ease: Expo.easeInOut
 });
+
+TweenMax.from(".mobilebanner", 4, {
+    opacity: 0,
+    delay: .5,
+    x: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".banner-thumbnail-content", 5, {
+    opacity: 0,
+    delay: .75,
+    x: -20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".bannercontent", 6, {
+    opacity: 0,
+    delay: 1,
+    y: -20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".appointmentcontent", 7, {
+    opacity: 0,
+    delay: 1.25,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".roundedwheel-header", 7, {
+    opacity: 0,
+    delay: 1.5,
+    x: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".mindlycharacteristics", 8, {
+    delay: 2,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".mindlypackages", 9, {
+    delay: 3,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".mindlydoctors", 10, {
+    delay: 4,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".achievepotential", 11, {
+    delay: 5,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".turnteam", 12, {
+    delay: 6,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".youthempowerment", 13, {
+    delay: 7,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".empower", 14, {
+    delay: 8,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".memberreview", 15, {
+    delay: 9,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".incrementcounter", 16, {
+    delay: 10,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".ourclients", 17, {
+    delay: 11,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".blogs", 18, {
+    delay: 12,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".press", 19, {
+    delay: 13,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
+TweenMax.staggerFrom(".footer", 20, {
+    delay: 14,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.08);
+
 
 // HOME PAGE END
