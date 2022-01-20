3b8e185f4b3400dffe3be5aabd139643b5e0a9ce
it27472 <istiak.tushar.qtec@gmail.com>
2022-01-20 16:51:46 +0600

    home all slider changed to swiper slider

--- assets/js/mindlycarousel.js
+++ assets/js/mindlycarousel.js
@@ -1,13 +1,160 @@
 
-// document.addEventListener( 'DOMContentLoaded', function() {
-    
-// });
-
-var splide = new Splide('.splide', {
-    type   : 'loop',
-    perPage: 2,
-    perMove: 1,
+/**======================================= **/
+//       MINDLY MOBILE CHARACTERSTICS       //
+/**======================================= **/
+var mobilecharacteristic = new Swiper(".swiper-mobilecharacteristic", {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+    },
+    breakpoints: {  
+        '499': {
+            slidesPerView: 1,
+        },
+        '500': {
+            slidesPerView: 2,
+            spaceBetween: 16
+        }
+    }
 });
 
+/**======================================== **/
+//              CLIENTS-REVIEW               //
+/**======================================== **/
+var homebrands = new Swiper(".swiper-clientreview", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    breakpoints: {  
+        '499': {
+            slidesPerView: 1
+        },
+        '768': {
+            slidesPerView: 2,
+            spaceBetween: 20
+        }
+    }
+});
 
-splide.mount();
+/**======================================== **/
+//              CLIENTS-SLIDER               //
+/**======================================== **/
+var homebrands = new Swiper(".swiper-homebrands", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    breakpoints: {  
+        '499': {
+            slidesPerView: 1
+        },
+        '500': {
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+        '850': {
+            slidesPerView: 3
+        },
+        '991': {
+            slidesPerView: 4
+        },
+        '1024': {
+            slidesPerView: 4
+        },
+        '1200': {
+            slidesPerView: 5,
+            spaceBetween: 16
+        },
+        '1400': {
+            slidesPerView: 6,
+            spaceBetween: 16
+        }
+    }
+});
+
+/**======================================== **/
+//              CLIENTS-SLIDER               //
+/**======================================== **/
+var mindlypackages = new Swiper(".swiper-mindlypackages", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {  
+        '499': {
+            slidesPerView: 1
+        },
+        '500': {
+            slidesPerView: 2,
+            spaceBetween: 12
+        },
+        '1024': {
+            slidesPerView: 3,
+            spaceBetween: 12
+        },
+    }
+});
+
+/**======================================== **/
+//                BLOG-SLIDER                //
+/**======================================== **/
+var mobileblogs = new Swiper(".swiper-mobileblogs", {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+    },
+    breakpoints: {  
+        '499': {
+            slidesPerView: 1,
+        },
+        '500': {
+            slidesPerView: 2,
+            spaceBetween: 16
+        }
+    }
+});
+
+/**======================================== **/
+//                PRESS-SLIDER               //
+/**======================================== **/
+var mobilepress = new Swiper(".swiper-mobilepress", {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    breakpoints: {  
+        '499': {
+            slidesPerView: 1,
+        },
+        '500': {
+            slidesPerView: 2,
+            spaceBetween: 16
+        }
+    }
+});
+
+/**======================================== **/
+//              MOBILE-DOCTORS               //
+/**======================================== **/
+var mobiledoctors = new Swiper(".swiper-mobiledoctors", {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    breakpoints: {  
+        '499': {
+            slidesPerView: 1,
+        },
+        '500': {
+            slidesPerView: 2,
+            spaceBetween: 16
+        }
+    }
+});
+    
