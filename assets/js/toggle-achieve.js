8ae020a002e0b7adc8285f099d2689a1fae3e447
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-16 16:13:55 +0600

    home dropdown completed

---
+++ assets/js/toggle-achieve.js
@@ -0,0 +1,14 @@
+// const achieves = Array.from(document.querySelectorAll('.desktopachieve'));
+
+// for(let i = 0; i < achieves.length; i++) {
+//     achieves[i].addEventListener('mouseover', function() {
+//         const achieveunlock = document.querySelector('.achieveunlock');
+
+//         if(achieveunlock.classList.contains('achieveactive')) {
+//             achieveunlock.classList.remove('achieveactive');
+//         } else {
+//             achieveunlock.classList.add('achieveactive');
+//         }
+//     });
+// }
+
